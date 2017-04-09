46b25b9664a81a217bde284eaa5cb8170080d2ec
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2017-04-09 22:07:22 +0200

    show P0 and P1 wihle waiting for players

--- pong.py
+++ pong.py
@@ -10,6 +10,8 @@ from ball import Ball
 from misc import Point
 from paddle import Paddle
 
+import drawing
+
 class Game(object):
     """
     The main game class that holds the gameloop.
@@ -23,7 +25,7 @@ class Game(object):
         self.clock = pymlgame.Clock(15)
         self.gameover = False
         self.players = {}
-        self.state = "WAITING"
+        self.state = "WAITING_FOR_PLAYERS"
         self.init_ball()
 
     def init_ball(self):
@@ -31,20 +33,44 @@ class Game(object):
         ball_velocity = Point(random.choice([-1, 1]) * 0.5, random.choice([-1, 1]) * 0.2)
         self.ball = Ball(ball_position, ball_velocity)
 
-    def update(self):
-        """
-        Update the screens contents in every loop.
-        """
-        if self.state == "WAITING":
-            pass
+    def update_and_render(self):
+        self.screen.reset()
+
+        if self.state == "WAITING_FOR_PLAYERS":
+            self.update_waiting()
+            self.render_waiting()
         elif self.state == "START_GAME":
             self.update_start_game()
         elif self.state == "RUNNING":
             self.update_running()
+            self.render_running()
         elif self.state == "GAME_OVER":
             self.update_game_over()
         else:
             print("That's simply poor programming...")
+
+        self.screen.update()
+        self.clock.tick()
+
+    def update_waiting(self):
+        if len(self.players) == 2:
+            self.state = "START_GAME"
+
+    def render_waiting(self):
+        p0_color = pymlgame.GREY5
+        p1_color = pymlgame.GREY5
+        if len(self.players) > 0:
+            p0_color = pymlgame.GREEN
+        if len(self.players) > 1:
+            p1_color = pymlgame.GREEN
+
+        surface = pymlgame.Surface(self.screen.width, self.screen.height)
+        # note(hrantzsch): this works for our default screen size only
+        drawing.draw_p(surface, Point(0, 2), p0_color)
+        drawing.draw_0(surface, Point(4, 2), p0_color)
+        drawing.draw_p(surface, Point(10, 2), p1_color)
+        drawing.draw_1(surface, Point(14, 2), p1_color)
+        self.screen.blit(surface)
 
     def update_start_game(self):
         time.sleep(2)
@@ -60,32 +86,24 @@ class Game(object):
         elif self.ball.position.x <= 0 or \
            self.ball.position.x >= self.screen.width-1:
             self.state = "GAME_OVER"
+            # todo(hrantzsch): we don't want to update the ball here -- refactor
         self.ball.update()
 
-    def update_game_over(self):
-        self.init_ball()
-        for p in self.players.values():
-            p.reset()
-        self.state = "START_GAME"
-
-    def render(self):
+    def render_running(self):
         """
         Send the current screen content to Mate Light.
         """
-        self.screen.reset()
-
         for paddle in self.players.values():
             self.screen.blit(paddle.surface, paddle.position)
 
         self.screen.blit(self.ball.surface,
                          tuple(map(round, self.ball.position)))
 
-        self.screen.update()
-        self.clock.tick()
-
-    def start_game(self):
+    def update_game_over(self):
+        self.init_ball()
+        for p in self.players.values():
+            p.reset()
         self.state = "START_GAME"
-        print("starting game")
 
     def construct_player(self, uid):
         self.players[uid] = Paddle(
@@ -94,8 +112,6 @@ class Game(object):
             is_first_player=len(self.players) == 0
         )
         print('new ctlr with uid:', uid)
-        if len(self.players) == 2:
-            self.start_game()
 
     def handle_events(self):
         """
@@ -122,8 +138,7 @@ class Game(object):
         try:
             while not self.gameover:
                 self.handle_events()
-                self.update()
-                self.render()
+                self.update_and_render()
         except KeyboardInterrupt:
             pass
 
