627775ca93f725b16f1bad8922c2b9aa1eab27eb
Hannes Rantzsch <gh@hannesrantzsch.de>
2017-04-11 21:43:45 +0200

    fancy state pattern for game states

---
+++ game_states.py
@@ -0,0 +1,98 @@
+import pymlgame
+import time
+
+import drawing
+from misc import Point
+
+
+class GameState():
+    """
+    Abstract base class for a state of a pyngpong game. Derived classes should
+    implement update() and render().
+    States will go ahead and replace themselves by another state if they deem
+    it necessary.
+    """
+
+    def __init__(self, game):
+        self.game = game
+
+    def update(self):
+        pass
+
+    def render(self):
+        pass
+
+
+class WaitingState(GameState):
+
+    def update(self):
+        if len(self.game.players) == 2:
+            self.game.state = StartingState(self.game)
+
+    def render(self):
+        p0_color = pymlgame.GREEN if len(self.game.players) > 0 \
+            else pymlgame.GREY5
+        p1_color = pymlgame.GREEN if len(self.game.players) > 1 \
+            else pymlgame.GREY5
+
+        surface = pymlgame.Surface(
+            self.game.screen.width, self.game.screen.height)
+        # note(hrantzsch): this works for our default screen size only
+        drawing.draw_p(surface, Point(0, 2), p0_color)
+        drawing.draw_0(surface, Point(4, 2), p0_color)
+        drawing.draw_p(surface, Point(10, 2), p1_color)
+        drawing.draw_1(surface, Point(14, 2), p1_color)
+        self.game.screen.blit(surface)
+
+
+class StartingState(GameState):
+
+    def update(self):
+        time.sleep(2)
+        self.game.state = RunningState(self.game)
+
+    def render(self):
+        # maybe a fancy countdown?
+        pass
+
+
+class RunningState(GameState):
+
+    def update(self):
+        if self.game.ball.position.x <= 0 or \
+           self.game.ball.position.x >= self.game.screen.width-1:
+            self.game.state = GameOverState(self.game)
+            return
+
+        if any([p.check_collision(self.game.ball)
+                for p in self.game.players.values()]):
+            self.game.ball.reflect("x")
+        # note: we can collide with the paddle AND collide with the ceiling
+        if self.game.ball.position.y <= 0 or \
+           self.game.ball.position.y >= self.game.screen.height-1:
+            self.game.ball.reflect("y")
+
+        self.game.ball.update()
+
+    def render(self):
+        """
+        Send the current screen content to Mate Light.
+        """
+        for paddle in self.game.players.values():
+            self.game.screen.blit(paddle.surface, paddle.position)
+
+        self.game.screen.blit(self.game.ball.surface,
+                              tuple(map(round, self.game.ball.position)))
+
+
+class GameOverState(GameState):
+
+    def update(self):
+        self.game.init_ball()
+        for p in self.game.players.values():
+            p.reset()
+        self.game.state = StartingState(self.game)
+
+    def render(self):
+        # maybe a fancy highscore screen of some kind?
+        pass
--- pong.py
+++ pong.py
@@ -1,107 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import copy
 import pymlgame
 import random
-import time
 
 from ball import Ball
 from misc import Point
 from paddle import Paddle
+from game_states import WaitingState
 
-import drawing
 
 class Game(object):
     """
     The main game class that holds the gameloop.
     """
+
     def __init__(self, mlhost, mlport, screen_width, screen_height):
         """
         Create a screen and define some game specific things.
         """
         pymlgame.init()
-        self.screen = pymlgame.Screen(mlhost, mlport, screen_width, screen_height)
+        self.screen = pymlgame.Screen(
+            mlhost, mlport, screen_width, screen_height)
         self.clock = pymlgame.Clock(15)
         self.gameover = False
         self.players = {}
-        self.state = "WAITING_FOR_PLAYERS"
+        self.state = WaitingState(self)
         self.init_ball()
 
     def init_ball(self):
         ball_position = Point(self.screen.width // 2, self.screen.height // 2)
-        ball_velocity = Point(random.choice([-1, 1]) * 0.5, random.choice([-1, 1]) * 0.2)
+        ball_velocity = Point(random.choice(
+            [-1, 1]) * 0.5, random.choice([-1, 1]) * 0.2)
         self.ball = Ball(ball_position, ball_velocity)
-
-    def update_and_render(self):
-        self.screen.reset()
-
-        if self.state == "WAITING_FOR_PLAYERS":
-            self.update_waiting()
-            self.render_waiting()
-        elif self.state == "START_GAME":
-            self.update_start_game()
-        elif self.state == "RUNNING":
-            self.update_running()
-            self.render_running()
-        elif self.state == "GAME_OVER":
-            self.update_game_over()
-        else:
-            print("That's simply poor programming...")
-
-        self.screen.update()
-        self.clock.tick()
-
-    def update_waiting(self):
-        if len(self.players) == 2:
-            self.state = "START_GAME"
-
-    def render_waiting(self):
-        p0_color = pymlgame.GREEN if len(self.players) > 0 else pymlgame.GREY5
-        p1_color = pymlgame.GREEN if len(self.players) > 1 else pymlgame.GREY5
-
-        surface = pymlgame.Surface(self.screen.width, self.screen.height)
-        # note(hrantzsch): this works for our default screen size only
-        drawing.draw_p(surface, Point(0, 2), p0_color)
-        drawing.draw_0(surface, Point(4, 2), p0_color)
-        drawing.draw_p(surface, Point(10, 2), p1_color)
-        drawing.draw_1(surface, Point(14, 2), p1_color)
-        self.screen.blit(surface)
-
-    def update_start_game(self):
-        time.sleep(2)
-        self.state = "RUNNING"
-
-    def update_running(self):
-        if self.ball.position.x <= 0 or \
-           self.ball.position.x >= self.screen.width-1:
-            self.state = "GAME_OVER"
-            return
-
-        if any([p.check_collision(self.ball) for p in self.players.values()]):
-            self.ball.reflect("x")
-        # note: we can collide with the paddle AND collide with the ceiling
-        if self.ball.position.y <= 0 or \
-           self.ball.position.y >= self.screen.height-1:
-            self.ball.reflect("y")
-
-        self.ball.update()
-
-    def render_running(self):
-        """
-        Send the current screen content to Mate Light.
-        """
-        for paddle in self.players.values():
-            self.screen.blit(paddle.surface, paddle.position)
-
-        self.screen.blit(self.ball.surface,
-                         tuple(map(round, self.ball.position)))
-
-    def update_game_over(self):
-        self.init_ball()
-        for p in self.players.values():
-            p.reset()
-        self.state = "START_GAME"
 
     def construct_player(self, uid):
         self.players[uid] = Paddle(
@@ -120,7 +51,8 @@ class Game(object):
                 if len(self.players) < 2:
                     self.construct_player(event.uid)
 
-            elif event.type == pymlgame.E_KEYDOWN or event.type == pymlgame.E_KEYPRESSED:
+            elif event.type == pymlgame.E_KEYDOWN or \
+                    event.type == pymlgame.E_KEYPRESSED:
                 if event.button == pymlgame.CTLR_UP:
                     self.players[event.uid].move_up()
                 elif event.button == pymlgame.CTLR_DOWN:
@@ -136,7 +68,11 @@ class Game(object):
         try:
             while not self.gameover:
                 self.handle_events()
-                self.update_and_render()
+                self.screen.reset()
+                self.state.update()
+                self.state.render()
+                self.screen.update()
+                self.clock.tick()
         except KeyboardInterrupt:
             pass
 
@@ -144,12 +80,17 @@ class Game(object):
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(description="Play Snake on your Matelight")
+    parser = argparse.ArgumentParser(
+        description="Play Snake on your Matelight")
     parser.add_argument('host', help='remote host to connect to')
-    parser.add_argument('-p', '--port', type=int, default=1337, help='remote port')
-    parser.add_argument('--width', type=int, default=15, help='width of matelight')
-    parser.add_argument('--height', type=int, default=16, help='height of matelight')
-    parser.add_argument('--demo', action='store_true', default=False, help='start game in demo mode')
+    parser.add_argument('-p', '--port', type=int,
+                        default=1337, help='remote port')
+    parser.add_argument('--width', type=int, default=15,
+                        help='width of matelight')
+    parser.add_argument('--height', type=int, default=16,
+                        help='height of matelight')
+    parser.add_argument('--demo', action='store_true',
+                        default=False, help='start game in demo mode')
 
     args = parser.parse_args()
     GAME = Game(
@@ -162,6 +103,5 @@ if __name__ == '__main__':
     if args.demo:
         GAME.construct_player(1)
         GAME.construct_player(2)
-        GAME.state = "START_GAME"
 
     GAME.gameloop()
